fbc725b79bc62b5642433554057afe92f59956cf
Emanuel Martinez <yinproducciones@gmail.com>
2025-05-20 17:19:48 -0300

    initial structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-darkBg text-darkText min-h-screen">
+      <h1 className="text-primary text-4xl">Mi Expense Tracker</h1>
+    </div>
+  )
+}
+
+export default App
