3981ae0fc9afe0f46e41b715d5819b130653b129
Emanuel Martinez <yinproducciones@gmail.com>
2025-10-04 19:10:29 -0300

    feat(UI): add figures on Auth Layout

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { LuTrendingUpDown } from 'react-icons/lu'
+import React from 'react';
+import { LuTrendingUpDown } from 'react-icons/lu';
+import CARD_1 from '../../assets/incomes-image.png';
 
 const AuthLayout = ({ children }) => {
   return (
@@ -10,20 +11,20 @@ const AuthLayout = ({ children }) => {
         </div>
 
         <div className='hidden md:block w-[40vw] h-screen bg-blue-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
-            <div className='w-48 rounded-[40px] bg-blue-600 absolute -top-7 -left-5'/>
-            <div className='w-48 rounded-[40px] border-[20px] border-blue-600 absolute top-[30%] -right-10'/>
-            <div className='w-48 rounded-[40px] bg-blue-500 absolute -bottom-7 -left-5'/>
+            <div className='w-48 h-48 rounded-[40px] bg-blue-600 absolute -top-7 -left-5'/>
+            <div className='w-48 h-56 rounded-[40px] border-[20px] border-blue-600 absolute top-[30%] -right-10'/>
+            <div className='w-48 h-48 rounded-[40px] bg-blue-500 absolute -bottom-7 -left-5'/>
 
             <div className='grid grid-cols-1 z-20'>
                 <StatsInfoCard 
                     icon={<LuTrendingUpDown />}
                     label='Track your Income & Expenses'
-                    value='430,000'
+                    value='220,000'
                     color='bg-primary'
                 />
             </div>
 
-            {/* <img className='w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15 ' src="" alt="" /> */}
+            <img className='w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15 ' src={CARD_1} alt="income-img"/>
         </div>
     </div>
   )
